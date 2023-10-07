4980b1371e6ad2fc0a79535de7a08ab35259dd36
Siddharthirkal <siddharthirkal06>
2023-10-07 13:26:21 +0530

    ExpensesAdded

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,11 @@
-function ExpenseItem() {
+import './ExpenseItem.css';
+function ExpenseItem(props) {
   return (
-    <div>
-      <div>Expense Items</div>
-      <div>
-        <h2>Food Rs 10</h2>
-        <h2>Petrol Rs 100</h2>
-        <h2>Movies Rs 200</h2>
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
